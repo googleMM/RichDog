6e7b70e751f2776c007dc02dea4d9275449036eb
vagrant <vagrant@dominos.com.au>
2014-11-26 07:44:45 -0800

    ziye : #001 added deploy task in grunt.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -2,17 +2,28 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    appDir: 'src',
+    deployDir: '/inetpub/wwwroot/workspace/richdog',
     uglify: {
       build: {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    copy: {
+      deploy: {
+        cwd: '<%= appDir %>',
+        expand: true,
+        src: [
+          '**/*'
+        ],
+        dest: '<%= deployDir %>'
+      }
     }
   });
-  var config = {
-    appDir: 'src',
-    defaultDeployDir: '/inetpub/wwwroot/workspace/richdog'
-  };
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['uglify']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['copy']);
 };
